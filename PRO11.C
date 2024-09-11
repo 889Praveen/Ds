e1cb0af0442cc6e2f54ce51e776bf933c151b792
Prajapati Praveen <prajapatipraveen1104@gmail.com>
2024-09-11 12:51:42 +0530

    Add files via upload

--- PRO11.C
+++ PRO11.C
@@ -1,126 +1,166 @@
-
 #include<stdio.h>
+#include<stdlib.h>
 #include<conio.h>
 struct node
 {
-   struct node *next;
-   int item;
+	int data;
+	struct node *next;
 };
-struct node* insert(struct node *head)
+
+
+struct node* insert(struct node *head,int ele)
 {
-  struct node *new1;
-  int data;
-  new1=(struct node *)malloc(sizeof(struct node));
-  printf("\nEnter Insert Ele:");
-  scanf("%d",&data);
-  if(head==NULL)
-  {
-    head=new1;
-    new1->item=data;
-    new1->next=NULL;
-  }
-  else
-  {
-    new1->item=data;
-    new1->next=head;
-    head=new1;
-  }
+	struct node *n,*prev,*curr;
+
+	n=(struct node*)malloc(sizeof(struct node));
+
+	n->data=ele;
+	prev=NULL;
+	curr=head;
+
+	if(head==NULL)
+	{
+
+		n->next=NULL;
+		head=n;
+
+	}
+	else
+	{
+
+		while( curr!=NULL && ele>curr->data)
+		{
+			prev=curr;
+			curr=curr->next;
+		}
+
+
+		if(head==curr)
+		{
+		 n->next=head;
+		 head=n;
+		}
+
+		else
+		{
+
+			prev->next=n;
+			n->next=curr;
+		}
+	}
+
   return head;
 }
-void dis(struct node *head)
-{
-  int i;
-  struct node *temp;
-  temp=head;
- if(temp==NULL)
- {
-  printf("\nLinkList is Empty..!");
- }
- else
- {
-   printf("\nLinklist:");
-   while(temp!=NULL)
-    {
-     printf(" %d ",temp->item);
-     temp=temp->next;
-    }
 
- }
-}
-struct node* remo(struct node *head)
+struct node* del(struct node *head,int ele)
 {
- struct node *temp;
- temp=head;
- if(head==NULL)
- {
-   printf("\nLinkList is Empty...!");
- }
- else
- {
-   head=head->next;
-   free(temp);
- }
- return head;
+	struct node *prev,*curr;
 
+	prev=NULL;
+	curr=head;
+
+
+	   while(curr!=NULL && ele>curr->data)
+	   {
+		   prev=curr;
+		   curr=curr->next;
+	   }
+	   if(head==curr)
+	   {
+		   head=curr->next;
+		   free(curr);
+	   }
+	   else
+	   {
+		   prev->next=curr->next;
+		   free(curr);
+	   }
+	return head;
 }
-void modify(struct node *head)
+
+int modify(struct node *head,int old,int newEl)
 {
-  struct node *temp=head;
-  int New,ele;
-  if(temp==NULL)
-  {
-   printf("\nLinklist is Empty..!");
-  }
-  else
-  {
-    printf("\nEnter Update Element:");
-    scanf("%d",&ele);
-    while(temp!=NULL)
-    {
-     if(temp->item==ele)
-      {
-	 printf("\nEnter New Element:");
-	 scanf("%d",&New);
-	  temp->item=New;
-	  break;
-      }
-    }temp=temp->next;
-  }
+    struct node *curr;
+    int i;
+    curr=head;
+   while(curr!=NULL && old!=curr->data)
+   {
+      curr=curr->next;
+   }
+   if(curr->data==old)
+   {
+    curr->data=newEl;
+     i=1;
+   }
+   else
+   {
+   i=0;
+   }
+   return i;
+}
+void display(struct node *head)
+{
+	struct node *temp;
+	temp=head;
+	if(temp==NULL)
+	{
+	 printf("LInklist is Empty..!");
+	}
+	else
+	{
+	while(temp!=NULL)
+	{
+		printf("%d ",temp->data);
+		temp=temp->next;
+	}
+	}
 }
 void main()
 {
-  int ch;
-  struct node *head=NULL;
-  clrscr();
+	struct node *head=NULL;
+	int data,c,newEl,old;
 
-  while(1)
-  {
-  printf("\nEnter 1 For Insert..");
-  printf("\nEnter 2 For Remove..");
-  printf("\nEnter 3 For Modify..");
-  printf("\nEnter 4 For Display..");
-  printf("\nEnter 5 Foe  Exit..");
-  printf("\nEnter choice:");
-  scanf("%d",&ch);
-  switch(ch)
-  {
-   case 1:
-	  head=insert(head);
-	  break;
-   case 5:
-	  exit(0);
-	  break;
-   case 4:
-	  dis(head);
-	  break;
-   case 2:
-	  head=remo(head);
-	  dis(head);
-	  break;
-   case 3:
-	  modify(head);
-	  break;
+	while(1)
+	{
+		printf("\nEnter One For Insert..");
+		printf("\nEnter Two For Delete..");
+		printf("\nEnter Three For Modify..");
+		printf("\nEnter Four For Display..");
+		printf("\nEnter Five For Exit..");
+		printf("\nEnter Chioce:");
+		scanf("%d",&c);
+		switch(c)
+		{
+		case 1:printf("\nEnter Element:");
+			   scanf("%d",&data);
+			   head=insert(head,data);
+			   display(head);
+			   break;
+		case 2:printf("\nEnter Element For Delete:");
+			   scanf("%d",&data);
+			   head=del(head,data);
+			   display(head);
+			   break;
+		case 3:printf("\nEnter Update Element:");
+		       scanf("%d",&old);
+		       printf("\n Enter new Element:");
+		       canf("%d",&newEl);
+		       if(modify(head,old,newEl))
+			 {
+			  printf("%d is Replace by %d ",old,newEl);
+			 }
+		       else
+			 {
+			  printf("Value is Not Available in Linklist");
+			 }
+		       printf("\n");
+		       display(head);
+		       break;
+	case 4:display(head);
+	       break;
+	case 5:exit(0);
+		}
+	}
 
-  }
-}
+	getch();
 }
